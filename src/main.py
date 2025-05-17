fd8dcb8f4adab4492500c3b9013ee33fb3c30b09
rkdaudtjr <kms0902k12@gmail.com>
2025-05-17 14:21:02 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from resources import labs_routes, main_routes, mypage_routes  # 라우터 impor
 from resources import auth_routes, explanation_routes
 from resources import explanation_routes, admin_routes
 from database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
@@ -15,6 +16,14 @@ app.include_router(explanation_routes.router, prefix="/subject") #해설 페이�
 app.include_router(labs_routes.router, prefix="/labs") #실습페이지 API
 app.include_router(admin_routes.router, prefix="/admin")   #관리자페이지 API
 
+# 🔥 CORS 설정 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],  # Vue 개발 서버 주소
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
