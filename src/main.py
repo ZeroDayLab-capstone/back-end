8cf28fed7eb57b7a60120ada1fab05d4756600c4
koko0917 <68803900+koko0917@users.noreply.github.com>
2025-05-28 23:02:34 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from resources import labs_routes, main_routes, mypage_routes  # 라우터 import
-from resources import auth_routes, explanation_routes, flag_routes
+from resources import labs_routes, mypage_routes  # 라우터 import
+from resources import auth_routes, flag_routes
 from resources import admin_routes
 from database import Base, engine
 from fastapi.middleware.cors import CORSMiddleware
@@ -12,10 +12,8 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 # 라우터 등록
-app.include_router(main_routes.router, prefix="/main")     # 메인 페이지 API
 app.include_router(mypage_routes.router, prefix="/mypage") # 마이페이지 API
 app.include_router(auth_routes.router, prefix="/auth")     # 로그인 API
-app.include_router(explanation_routes.router, prefix="/subject") # 해설 페이지 API
 app.include_router(labs_routes.router, prefix="/labs")     # 실습페이지 API
 app.include_router(admin_routes.router, prefix="/admin")   # 관리자페이지 API
 app.include_router(flag_routes.router, prefix="/flag")     # 플래그 제출 API
