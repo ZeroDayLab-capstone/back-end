b24b87235ce8e20f444f3c8b0624e56b35af99c0
koko0917 <68803900+koko0917@users.noreply.github.com>
2025-05-29 11:17:40 +0900

    Update container_routes.py

--- src/container_routes.py
+++ src/container_routes.py
@@ -27,7 +27,7 @@ PROBLEM_CONFIG: Dict[int, Dict[str, object]] = {
     4: {"frontend_image": "zeroday01478/command:frontend",  "backend_image": "zeroday01478/command:backend",  "host_range": (2000, 6000), "flag": "<여기에 command 플래그>"},
     5: {"frontend_image": "zeroday01478/stored_xss_1:frontend", "backend_image": "zeroday01478/stored_xss_1:backend", "host_range": (2000, 6000), "flag": "<여기에 stored_xss_1 플래그>"},
     6: {"frontend_image": "zeroday01478/stored_xss_2:frontend", "backend_image": "zeroday01478/stored_xss_2:backend", "host_range": (2000, 6000), "flag": "<여기에 stored_xss_2 플래그>"},
-    7: {"frontend_image": "zeroday01478/stored_xss_3-frontend:latest", "backend_image": "zeroday01478/stored_xss_3-backend:latest", "host_range": (2000, 6000), "flag": "<여기에 stored_xss_3 플래그>"},
+    7: {"frontend_image": "zeroday01478/stored_xss_3:frontend", "backend_image": "zeroday01478/stored_xss_3:backend", "host_range": (2000, 6000), "flag": "<여기에 stored_xss_3 플래그>"},
     8: {"frontend_image": "zeroday01478/reflected_xss:frontend","backend_image": "zeroday01478/reflected_xss:backend","host_range": (2000, 6000), "flag": "<여기에 reflected_xss 플래그>"},
     9: {"frontend_image": "zeroday01478/file-upload:frontend",  "backend_image": "zeroday01478/file-upload:backend",  "host_range": (2000, 6000), "flag": "<여기에 file-upload 플래그>"},
    10: {"frontend_image": "zeroday01478/path-traversal:frontend","backend_image": "zeroday01478/path-traversal:backend","host_range": (2000, 6000), "flag": "<여기에 path-traversal 플래그>"},
